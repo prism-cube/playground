6b961fda01938198e79aa88556e0d49ea74c275b
prism-cube <ocean5014dev@yahoo.co.jp>
2021-12-23 23:38:59 +0900

    gtag

--- src/utils/gtag.ts
+++ src/utils/gtag.ts
@@ -1,10 +1,9 @@
 export const gaMeasurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || ''
 
-// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const pageview = (url: string) => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-  window.gtag('config', gaMeasurementId, {
-    page_path: url,
-  })
+  if (window && window.gtag) {
+    window.gtag('config', gaMeasurementId, {
+      page_path: url,
+    });
+  }
 }
